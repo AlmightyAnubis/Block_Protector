10f228511677b784ba3a29f857ca0e045f3e1f3d
Almighty_Anubis <dereinstein80@gmail.com>
2023-10-18 15:20:04 +0200

    Split up config for debugging

--- src/main/java/com/anubis/break_protection/configs/Config.java
+++ src/main/java/com/anubis/break_protection/configs/Config.java
@@ -2,29 +2,33 @@ package com.anubis.break_protection.configs;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.IForgeRegistry;
 
 public class Config {
-	
+
 	private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
-    public static final General GENERAL = new General(BUILDER);
-	
+	public static final General GENERAL = new General(BUILDER);
+
 	public static class General {
-		
+
 		public final ForgeConfigSpec.ConfigValue<List<? extends String>> protectedBlocks;
 
-		
 		public General(ForgeConfigSpec.Builder builder) {
 			builder.push("General");
-			
+
 			ArrayList<String> protBlockDefault = new ArrayList<String>();
-			protBlockDefault.add(ForgeRegistries.BLOCKS.getResourceKey(Blocks.BUDDING_AMETHYST).get().location().toString());
-			protectedBlocks = builder
-					.comment("List of protected blocks")
-					.translation("config.protectedBlocks")
+			IForgeRegistry<Block> registry = ForgeRegistries.BLOCKS;
+			Block block = Blocks.BUDDING_AMETHYST;
+			Optional<ResourceKey<Block>> key = registry.getResourceKey(block);
+			protBlockDefault.add(key.get().location().toString());
+			protectedBlocks = builder.comment("List of protected blocks").translation("config.protectedBlocks")
 					.define("protectedBlocks", protBlockDefault);
 
 			builder.pop();
@@ -35,5 +39,3 @@ public class Config {
 }
 
 //piglin_loved
-
-
